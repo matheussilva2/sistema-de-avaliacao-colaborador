9a9e26eabad6d82a4b71478e8d87b03d31047b32
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-08 00:58:45 -0300

    feat: criação do cabeçalho de colaborador

---
+++ web/src/components/CabecalhoColaborador/index.tsx
@@ -0,0 +1,10 @@
+const CabecalhoColaborador = () => {
+    return (
+        <header
+            className="h-20 border-b border-gray-400 text-lg font-semibold flex items-center px-6 bg-white">
+            <a href="#">Capacita NEES</a>
+        </header>
+    );
+}
+
+export default CabecalhoColaborador;
