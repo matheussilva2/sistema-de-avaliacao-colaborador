ff5feda81e4e43d03872737a61029b4d6ddc2d1f
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-08 00:58:56 -0300

    feat: base da página de login

---
+++ web/src/pages/Entrar/index.tsx
@@ -0,0 +1,13 @@
+import CabecalhoColaborador from "../../components/CabecalhoColaborador";
+
+export default function() {
+    return (
+        <>
+            <CabecalhoColaborador />
+            <main>
+                Hello world
+                {/* Formulário login */}
+            </main>
+        </>
+    );
+}
