57b58e8a533117d5acfe79cf1ee2720a33467bce
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-15 20:31:39 -0300

    feat: implementação do react router dom e configuração de rotas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css'
-import Entrar from "./pages/Entrar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import Template from "./components/Template";
+import Login from "./pages/Login";
 
 function App() {
 
   return (
-    <Entrar />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="painel" element={<Template titulo="TESTE" />}>
+          <Route index element={<>Início</>} />
+          <Route path="meu-perfil" element={<>Perfil</>} />
+          <Route path="colaboradores" element={<>Colaboradores</>} />
+          <Route path="treinamentos" element={<>Treinamentos</>} />
+          <Route path="gerenciar-treinamentos" element={<>Gerenciar Treinamentos</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
