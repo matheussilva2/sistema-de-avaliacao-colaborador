c74aceb8d6e1b29530533f83a7d9e2955666dad7
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-16 22:16:18 -0300

    feat: tornar password opcional no User type

--- web/src/types/User.ts
+++ web/src/types/User.ts
@@ -2,7 +2,7 @@ export type User = {
     nome: string;
     sobrenome: string;
     email: string;
-    password: string;
+    password?: string;
     foto_perfil: string;
     cargo: string;
     permissoes: string[]
