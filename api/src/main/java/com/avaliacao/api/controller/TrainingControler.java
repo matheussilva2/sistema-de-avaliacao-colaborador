9c5268d1fd9482d0d208bbb65993680ecfab8977
PedroGomes <pcabralgomes28@gmail.com>
2026-05-10 00:32:17 -0300

    Chore: CRUD completo para traing e ocutação de logs não funciona

--- api/src/main/java/com/avaliacao/api/controller/TrainingControler.java
+++ api/src/main/java/com/avaliacao/api/controller/TrainingControler.java
@@ -9,9 +9,14 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+// chore metodos http completos para trining
 
 @RestController
 public class TrainingControler {
@@ -36,10 +41,51 @@ public class TrainingControler {
         return ResponseEntity.status(HttpStatus.CREATED).body(trainingRepository.save(trainingModel));
     }
 
+    // implementação do méto do GetALL
+    // Lista todos os produtos que temos na base
 
+    @GetMapping("/trainings")
+    public ResponseEntity<List<TrainingModel>> getAllTrainings(){
+        return ResponseEntity.status(HttpStatus.OK).body(trainingRepository.findAll());
+    }
 
+    // GetOne -> Pegar um único recurs específico
 
+    @GetMapping("/trainings/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
+        Optional<TrainingModel> trainingO = trainingRepository.findById(id);
+        if(trainingO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Training not found.");
+        }
 
+        return ResponseEntity.status(HttpStatus.OK).body(trainingO.get());
+    }
 
+    // meto do PUT para atualizar um produto baseado no ID
 
+    @PutMapping("/trainings/{id}")
+
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+                                  @RequestBody @Valid TrainingRecordDTO trainingRecordDTO){
+        Optional<TrainingModel> tainingO = trainingRepository.findById(id);
+        if(tainingO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Training not found.");
+        }
+
+        var trainigModel = tainingO.get();
+        BeanUtils.copyProperties(trainingRecordDTO,trainigModel);
+        return ResponseEntity.status(HttpStatus.OK).body(trainingRepository.save(trainigModel));
+    }
+
+    // Meto do DELET
+
+    @DeleteMapping("/trainings/{id}")
+    public ResponseEntity<Object> deleteTraining(@PathVariable(value = "id") UUID id){
+        Optional<TrainingModel> triningO = trainingRepository.findById(id);
+        if(triningO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Training not found.");
+        }
+        trainingRepository.delete(triningO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Training deleted successfully");
+    }
 }
